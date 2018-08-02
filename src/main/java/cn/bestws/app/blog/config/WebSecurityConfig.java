77710ce78953d565563416adf7ef7ea0d6ce8d17
yaochenfeng <282696845@qq.com>
2018-08-02 17:01:13 +0800

    关闭 httpbasic

---
+++ src/main/java/cn/bestws/app/blog/config/WebSecurityConfig.java
@@ -0,0 +1,14 @@
+package cn.bestws.app.blog.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                .httpBasic().disable();
+    }
+}
