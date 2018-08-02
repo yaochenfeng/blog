ce835108899afbc9f2458aa00e203adffedfd044
yaochenfeng <282696845@qq.com>
2018-08-02 12:57:43 +0800

    add cros

---
+++ src/main/java/cn/bestws/app/blog/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package cn.bestws.app.blog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .maxAge(3600)
+                .allowCredentials(true);
+    }
+}
